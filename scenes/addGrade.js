1fd87ccf4fd0e5ddf1836489546af7740db421db
Максим Богинич <maksimboginic@Maksims-MacBook-Pro.local>
2021-06-20 05:09:36 +0300

    logs3

--- scenes/addGrade.js
+++ scenes/addGrade.js
@@ -44,6 +44,7 @@ const gradeDataWizard = new WizardScene(
             if (checkResponse.length == 0){
                 console.log('+')
                 await ctx.reply('Оцени Торговую идею:\nПортфель/Тикер/Торговый тезис', Keyboards.insertGrade());
+                console.log('+')
                 return ctx.wizard.next();
             }
             else {
